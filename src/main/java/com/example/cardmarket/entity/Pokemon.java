cb8186da2447d74bcabdd0029b7bff61fcb233a2
Cihat Erdem <cihat_erdem@outlook.com>
2022-12-12 16:15:14 +0100

    adw

--- src/main/java/com/example/cardmarket/entity/Pokemon.java
+++ src/main/java/com/example/cardmarket/entity/Pokemon.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Pokemon { //CardType
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String type;
